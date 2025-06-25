a040d8bacced65bd8e3d5a4c53d7e3ed88aa632a
Yu Chun,Chou <a830144@gmail.com>
2025-06-25 13:30:59 -0500

    for CORS

--- src/main/java/com/mysap/sd/priceguide/controller/CustomerPriceController.java
+++ src/main/java/com/mysap/sd/priceguide/controller/CustomerPriceController.java
@@ -2,6 +2,7 @@ package com.mysap.sd.priceguide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ public class CustomerPriceController {
     @Autowired
     private CustomerPriceService service;
 
+    @CrossOrigin(origins = "*") // or specific origin
     @GetMapping("/{productId}/{customerId}")
     public ResponseEntity<CustomerPrice> getPrice(
             @PathVariable String productId,
--- src/main/java/com/mysap/sd/priceguide/controller/StandardPriceController.java
+++ src/main/java/com/mysap/sd/priceguide/controller/StandardPriceController.java
@@ -2,6 +2,7 @@ package com.mysap.sd.priceguide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ public class StandardPriceController {
         return ResponseEntity.ok(service.update(id, price));
     }
 
+    @CrossOrigin(origins = "*") // or specific origin
     @GetMapping("/priceguide/{productId}")
     public ResponseEntity<StandardPrice> getByProductId(@PathVariable Long productId) {
         return service.getByProductId(productId)
